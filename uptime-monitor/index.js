df74b07a578f7883d3991f6daabd889eeecbaee1
kaypee <resaba.kaypee@gmail.com>
2020-11-05 20:24:17 +0700

    parse payloads

--- uptime-monitor/index.js
+++ uptime-monitor/index.js
@@ -1,6 +1,7 @@
 // node dependencies
 const http = require("http");
 const url = require("url");
+const { StringDecoder } = require("string_decoder");
 
 // the server should respond to all request with a string
 const server = http.createServer((req, res) => {
@@ -15,7 +16,7 @@ const server = http.createServer((req, res) => {
   const trimmedPath = path.replace(/^\/+|\/+$/g, "");
 
   // get query string as an object
-  const queryStringObj = parsedUrl.query;
+  const queryStringObj = JSON.stringify(parsedUrl.query);
 
   // get headers as an object
   const headers = req.headers;
@@ -23,10 +24,20 @@ const server = http.createServer((req, res) => {
   // get the method
   const method = req.method.toUpperCase();
 
-  // send the response
-  res.end("Hello world!\n");
+  // get payload if there is any
+  const decoder = new StringDecoder("utf-8");
+  let buffer = "";
 
-  console.log(headers);
+  // stream data that is coming in bit by bit
+  req.on("data", (data) => {
+    buffer += decoder.write(data);
+  });
+
+  req.on("end", () => {
+    // send the response
+    res.end("Hello world!\n");
+    console.log(buffer);
+  });
 });
 
 // start the server and have it listen to port 3000
